4c189de42760739f41b57c731f5810eab95c61a9
Suzuyu <akurai4107@gmail.com>
2022-10-22 01:53:10 +0900

    フェードイン・アウト追加

--- Assets/Scripts/Manager/SceneLoadManager.cs
+++ Assets/Scripts/Manager/SceneLoadManager.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 using static SceneConst;
 
 public class SceneLoadManager : MonoBehaviour
 {
+    private static readonly string _fadeLayerName = "Fade";
+
+    [SerializeField] private Canvas _canvas = null;
+    [SerializeField] private CanvasGroup _canvasGroup = null;
+    [SerializeField] private float _fadeSpeed = 1.0f;
+
     public static SceneLoadManager Instance = null;
 
     public SceneType PrevScene { get; private set; } = SceneType.Title;
     public SceneType CurrentScene { get; private set; } = SceneType.Title;
+
+    public bool IsFade { get; private set; } = false;
+
+    private bool _isAnimation = false;
 
 
     private void Awake()
@@ -19,12 +30,70 @@ public class SceneLoadManager : MonoBehaviour
         {
             Instance = this;
         }
+
+        _canvas.worldCamera = Camera.main;
+        _canvas.sortingLayerName = _fadeLayerName;
+        _canvasGroup.alpha = 0.0f;
+        _canvas.gameObject.SetActive(false);
     }
 
     public void ChangeScene(SceneType sceneType, bool isFade = true)
     {
+        ChangeSceneAsync(sceneType, isFade).Forget();
+    }
+
+    private async UniTask ChangeSceneAsync(SceneType sceneType, bool isFade)
+    {
+        _canvas.gameObject.SetActive(true);
+
+        if(_canvas.worldCamera == null)
+        {
+            _canvas.worldCamera = Camera.main;
+            _canvas.sortingLayerName = _fadeLayerName;
+        }
+
+        if(isFade)
+        {
+            await FadeIn();
+        }
+
         PrevScene = CurrentScene;
         CurrentScene = sceneType;
         SceneManager.LoadScene(sceneType.ToString());
+
+        if(isFade)
+        {
+            await FadeOut();
+        }
+
+        _canvas.gameObject.SetActive(false);
+    }
+
+    private async UniTask FadeIn()
+    {
+        IsFade = true;
+        _isAnimation = true;
+
+        await UniTask.WaitUntil(() =>
+        {
+            _canvasGroup.alpha += _fadeSpeed * Time.deltaTime;
+            return _canvasGroup.alpha >= 1.0f;
+        });
+
+        _isAnimation = false;
+    }
+
+    private async UniTask FadeOut()
+    {
+        _isAnimation = true;
+
+        await UniTask.WaitUntil(() =>
+        {
+            _canvasGroup.alpha -= _fadeSpeed * Time.deltaTime;
+            return _canvasGroup.alpha <= 0.0f;
+        });
+
+        _isAnimation = false;
+        IsFade = false;
     }
 }
